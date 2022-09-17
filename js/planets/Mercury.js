dd04bd2926b3db29b4654b20751de68c213ac706
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2022-09-17 22:20:01 +0200

    size scale to earth

--- js/planets/Mercury.js
+++ js/planets/Mercury.js
@@ -2,7 +2,7 @@ import * as THREE from '../../node_modules/three/build/three.module.js';
 
 export default class Mercury {
     constructor() {
-        const geom = new THREE.SphereGeometry(15, 64, 64)
+        const geom = new THREE.SphereGeometry(1, 64, 64)
         const mat = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../../map/mercury-map.jpg')})
         this.mercury = new THREE.Mesh(geom, mat)
     }
