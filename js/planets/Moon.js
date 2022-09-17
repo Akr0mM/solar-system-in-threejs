69446210a7c243efbc2417fb6a529118d9189281
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2022-09-17 22:20:16 +0200

    orbit and size to earth

--- js/planets/Moon.js
+++ js/planets/Moon.js
@@ -2,7 +2,7 @@ import * as THREE from '../../node_modules/three/build/three.module.js';
 
 export default class Moon {
     constructor() {
-        const geom = new THREE.SphereGeometry(0.5, 64, 64)
+        const geom = new THREE.SphereGeometry(0.272, 64, 64)
         const mat = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../../map/moon-map.jpg')})
         this.moon = new THREE.Mesh(geom, mat)
     }
