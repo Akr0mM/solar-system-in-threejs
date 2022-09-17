1023ccadc1f46a313e71cbff8cb5c78406622727
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2022-09-17 22:20:29 +0200

    size scale to earth

--- js/planets/Sun.js
+++ js/planets/Sun.js
@@ -2,7 +2,7 @@ import * as THREE from '../../node_modules/three/build/three.module.js';
 
 export default class Sun {
     constructor() {
-        const geom = new THREE.SphereGeometry(1.5, 64, 64)
+        const geom = new THREE.SphereGeometry(5, 64, 64)
         const mat = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../../map/sun-map.jpg')})
         this.sun = new THREE.Mesh(geom, mat)
     }
