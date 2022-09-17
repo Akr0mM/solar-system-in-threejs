5cac70721777b0cfa1d60275818d2ea3f00396ce
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2022-09-17 22:19:38 +0200

    size scale to earth: 1

--- js/planets/Earth.js
+++ js/planets/Earth.js
@@ -2,12 +2,20 @@ import * as THREE from '../../node_modules/three/build/three.module.js';
 
 export default class Earth {
     constructor() {
-        const geom = new THREE.SphereGeometry(1, 64, 64)
-        const mat = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../../map/earth-map.jpg')})
-        this.earth = new THREE.Mesh(geom, mat)
+        const geom = new THREE.SphereGeometry(1, 64, 64);
+        const mat = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../../map/earth-map.jpg') });
+        this.earth = new THREE.Mesh(geom, mat);
     }
 
     getMesh() {
-        return this.earth
+        return this.earth;
+    }
+
+    createLine() {
+        const geom = new THREE.RingGeometry(this.earth.position.x + 0.03, this.earth.position.x, 64);
+        const material = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+        const mesh = new THREE.Mesh(geom, material);
+        mesh.rotation.x += Math.PI / 2
+        return mesh;
     }
 }
